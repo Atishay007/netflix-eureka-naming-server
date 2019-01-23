6cd3ed984c1156728c96ba76a90b6c3e9ff034d9
Atishay Jain <atishayjain23@gmail.com>
2019-01-23 09:10:23 +0530

    First commit

---
+++ src/main/java/com/spring/limits/microservices/NetflixEurekaNamingServerApplication.java
@@ -0,0 +1,17 @@
+package com.spring.limits.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//To enable eureka server configuration.
+@EnableEurekaServer
+public class NetflixEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixEurekaNamingServerApplication.class, args);
+	}
+
+}
+
